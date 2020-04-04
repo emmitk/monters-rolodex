07b90b49a3e3489fce40effeb708697563e63414
Emmit Kadayifci <emmit@opensoft.com.au>
2020-04-04 23:42:34 +1100

    Updates

---
+++ precache-manifest.7a1ef078d30168025a164cce91d04b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bec4b2c0f01c4b731f1bc930b89939",
+    "url": "/monters-rolodex/index.html"
+  },
+  {
+    "revision": "762af14ed8641383a56f",
+    "url": "/monters-rolodex/static/css/main.125a53da.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762af14ed8641383a56f",
+    "url": "/monters-rolodex/static/js/main.7fbfcea6.chunk.js"
+  },
+  {
+    "revision": "d675114f4bc5dcac6e2b",
+    "url": "/monters-rolodex/static/js/runtime-main.e042b115.js"
+  }
+]);
